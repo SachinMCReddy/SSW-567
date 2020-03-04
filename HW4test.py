b1c0b6077f4259314c835f3e58f6333ae715791e
SachinMCReddy <47071453+SachinMCReddy@users.noreply.github.com>
2020-03-04 09:56:55 -0500

    Add files via upload

---
+++ HW4test.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  26 09:30:09 2020
+
+@author: sachinmcreddy
+"""
+
+import unittest
+
+from HW4567 import repo_info
+
+
+class TestGetInfo(unittest.TestCase):
+    def test_normal_response(self):
+        expected = ['User: SachinMCReddy',
+                    'Repo: test1 Number of commits: 4',
+                    'Repo: test2 Number of commits: 2',
+                    'Repo: Triangle567 Number of commits: 3']
+        self.assertEqual(repo_info(), expected)
+
+    def test_bad_user_name(self):
+        self.assertEqual(repo_info('xxxxxxx'), 'not able to fetch repos from user')
+        self.assertEqual(repo_info(''), 'not able to fetch repos from user')
+
+
+if __name__ == '__main__':
+    unittest.main()
